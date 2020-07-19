e6b0694debf4a96cd2530f1688d6f8f7ff991f3f
Vicente Abse <vicente.abse@gmail..com>
2020-07-18 22:17:37 -0400

    get despensa ahora si

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import http from 'http'
 import { env, port, ip, apiRoot } from './config'
 import express from './services/express'
 import api from './api'
-
 const app = express(apiRoot, api)
 const server = http.createServer(app)
 
@@ -17,6 +16,8 @@ setImmediate(() => {
     console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
   })
 })
+
+
 
 export default app
 
